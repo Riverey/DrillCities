bc7c16c1342bb839cbfdbda64281cbc6fcad383c
Georgy Rakita <rakitaegor@gmail.com>
2020-05-06 20:52:49 +0100

    Small changes to building module

---
+++ Assets/Scripts/Buildings/BuildingModule.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+/// <summary>
+/// Class used to spawn and manage modules of a building
+/// </summary>
+public class BuildingModule : MonoBehaviour
+{
+   
+    [SerializeField]
+    public List<BuildingModuleVariant> variants;
+
+    [System.Serializable]
+    public class BuildingModuleVariant
+    {
+        public string name;
+        public GameObject objectModel;
+
+        [Range(0, 100)]
+        public int spawnChanceModifier = 50;
+    }
+
+    public bool respondToEthicsChange = false; //used to determine if this module should be included in ethics recalculation
+
+    public GameObject currentModule;
+
+}
